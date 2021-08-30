7639d5de803581a0c0c3a3a552d40a55de1e1302
antoniovenezia <antoniovenezia00@gmail.com>
2021-08-30 17:56:03 +0200

    first milestone

--- firstMilestone.php
+++ firstMilestone.php
@@ -4,7 +4,7 @@ i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
 
 <?php
 
-require __DIR__ 'arrayDischi.php';
+require __DIR__ . '/arrayDischi.php';
 
 foreach($database as $disco){
     echo $disco['title'] . '<br>' . $disco['genre'] . '<br>' . $disco['year'] . '<br>';
